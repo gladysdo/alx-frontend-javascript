d05ee645d65761b8720fa510742ad658e0e9fc6b
gladysdo <doe.glad@gmail.com>
2024-03-09 12:36:49 +0000

    Promise resolves

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,11 +1,11 @@
 export default function handleResponseFromAPI(promise) {
   return promise
     .then((result) => {
-      console.log('Got a response from the API');
+      console.log('Got a successful response from the API');
       return { status: 200, body: 'success' };
     })
     .catch((error) => {
-      console.log('Got a response from the API');
-      return new Error();
+      console.log('Got an error response from the API');
+      return new Error('Something went wrong');
     });
 }
