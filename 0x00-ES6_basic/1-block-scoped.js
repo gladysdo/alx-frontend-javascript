b675cbe9fb6798a5efb8aa188eece951b1416a33
gladysdo <doe.glad@gmail.com>
2024-03-09 09:02:59 +0000

    Block Scope

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,10 +1,10 @@
 export default function taskBlock(trueOrFalse) {
   let task = false;
-  const task2 = true;
+  let task2 = true;
 
   if (trueOrFalse) {
     let task = true; // This creates a new variable in the block scope
-    const task2 = false; // This creates a new constant in the block scope
+    let task2 = false; // This creates a new variable in the block scope
   }
 
   return [task, task2];
