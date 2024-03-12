20ec29ae3f4e394d8bbcc9373a88c7e00e040b73
gladysdo <doe.glad@gmail.com>
2024-03-12 22:21:52 +0000

    Filter

---
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -0,0 +1,9 @@
+function getStudentsByLocation(studentList, city) {
+  // Use the filter function to get students located in the specified city
+  const studentsInCity = studentList.filter(student => student.location === city);
+
+  // Return the array of students in the specified city
+  return studentsInCity;
+}
+
+export default getStudentsByLocation;
