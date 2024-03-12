95d81858fca94f0717674c5b934febf3873901f3
gladysdo <doe.glad@gmail.com>
2024-03-12 22:10:24 +0000

    More mapping

---
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -0,0 +1,14 @@
+function getListStudentIds(studentList) {
+  // Check if the provided argument is an array
+  if (!Array.isArray(studentList)) {
+    return [];
+  }
+
+  // Use the map function to extract the 'id' from each student object
+  const studentIds = studentList.map(student => student.id);
+
+  // Return the array of student ids
+  return studentIds;
+}
+
+export default getListStudentIds;
