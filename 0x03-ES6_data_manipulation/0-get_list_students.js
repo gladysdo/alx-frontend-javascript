1011064a9292281bc7701d79b42c98c84b234a5c
gladysdo <doe.glad@gmail.com>
2024-03-12 22:05:54 +0000

    Basic list of objects

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,15 @@
+// Define a function named getListStudents
+function getListStudents() {
+  // Create an array of objects representing students
+  let students = [
+    { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+    { id: 2, firstName: 'James', location: 'Columbia' },
+    { id: 5, firstName: 'Serena', location: 'San Francisco' }
+  ];
+
+  // Return the array of students
+  return students;
+}
+
+// Export the getListStudents function to make it available for other files
+export default getListStudents;
