8ecb977013d6057799ff652dc705730c76cc1a2d
gladysdo <doe.glad@gmail.com>
2024-03-13 00:25:56 +0000

    Clean set

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,6 +1,6 @@
 function cleanSet(set, startString) {
   // Filter the set values that start with the specified string
-  const filteredValues = Array.from(set).filter(value => value.startsWith(startString));
+  const filteredValues = Array.from(set).filter((value) => value.startsWith(startString));
 
   // Concatenate the filtered values with '-' separator
   const resultString = filteredValues.join('-');
